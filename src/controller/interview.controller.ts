470de18cafc9cdf9b79e669f5c1eba557b309d47
dushant <dushantbanpurkar@gmail.com>
2026-09-10 01:02:24 +0530

    CreateInterviewSession Controller Code Overview: 1. Pre processing added for Zod, so data format convert for Zod validation.2. Zod validation Schema added early. 3. Destructure fields cleanly out of the validated result data object. 4. File proccessing layer separated, so it can process safely and cleanly. 5. Database Interaction Layer.

--- src/controller/interview.controller.ts
+++ src/controller/interview.controller.ts
@@ -1,83 +1,101 @@
-import {Request,Response} from "express"
+import { Request, Response } from "express";
 import { extractTextFromArrayBuffer } from "../helper/pdfToText";
-import {InterviewModel} from '../model/interview.model';
-import multer from "multer"
+import { InterviewModel } from "../model/interview.model";
+import { CreateInterviewSchema } from "../schemas/interview.schema";
 
 interface MulterRequest extends Request {
   file?: globalThis.Express.Multer.File;
 }
 
-export async function createInterviewSession(req:MulterRequest,res:Response):Promise<void> {
-    try{
-        const {
-            candidateId,
-            roleTitle,
-            targetSkills,
-            jobDescription,
-            difficultyMode='medium',
-            enableSandbox=true,
-            allowedLanguages=['javascript','python','cpp']
-        }=req.body;
+export async function createInterviewSession(
+  req: MulterRequest,
+  res: Response,
+): Promise<void> {
+  try {
+    let parsedSkills: string[] = [];
 
-        if(!candidateId||!roleTitle||!jobDescription){
-            res.status(400).json({error:"candiateId, roleTitle, and jobDescription"});
-            return;
-        }
-
-        let extractedResumeText='';
-        if(req.file){
-            const unitArrayData=new Uint8Array(req.file.buffer);
-            // const arrayBuffer=buffer.buffer.slice(
-            //     buffer.byteOffset,
-            //     buffer.byteOffset+ buffer.byteLength
-            // )
-
-            const textData=await extractTextFromArrayBuffer(unitArrayData);
-
-            if(!textData){
-                console.warn("Warning: extractTextFromArrayBuffer returned empty or null data.")
-            }
-            else{
-                extractedResumeText=textData;
-            }
-        }
-
-        let parsedSkills: string[]=[];
-
-        if(targetSkills){
-            parsedSkills=typeof targetSkills==='string' ?JSON.parse(targetSkills) : targetSkills;
-        }
-
-        let parsedLanguages:string[]=allowedLanguages;
-        if(typeof allowedLanguages==='string'){
-            parsedLanguages=JSON.parse(allowedLanguages);
-        }
-
-        const interview=await InterviewModel.create({
-            candidateId,
-            roleTitle,
-            targetSkills:parsedSkills,
-            resumeText:extractedResumeText,
-            jobDescription,
-            difficultyMode,
-            enableSandbox:String(enableSandbox)==='true',
-            allowedLanguages:parsedLanguages,
-            status:'scheduled'
-        })
-
-        res.status(201).json({
-            message:'Interview session created successfully with uploaded resume.',
-            interviewId:interview._id,
-            config:{
-                difficultyMode:interview.difficultyMode,
-                enableSandbox:interview.enableSandbox,
-                allowedLanguages:interview.allowedLanguages,
-            }
-        })
-
+    if (req.body.targetSkills) {
+      parsedSkills =
+        typeof req.body.targetSkills === "string"
+          ? JSON.parse(req.body.targetSkills)
+          : req.body.targetSkills;
     }
-    catch(error:any){
-        console.error("Error in createInterviewSession: ",error.message);
-        res.status(500).json({error:'Failed to create interview session',details:error.message})
+
+    let parsedLanguages: string[] = req.body.allowedLanguages;
+    if (typeof req.body.allowedLanguages === "string") {
+      parsedLanguages = JSON.parse(req.body.allowedLanguages);
     }
-}
+
+    const parsedEnableSandox =
+      req.body.enableSandbox !== undefined
+        ? String(req.body.enableSandbox) === "true"
+        : true;
+
+    const payloadToValidate = {
+      ...req.body,
+      targetSkills: parsedSkills,
+      allowedLanguages: parsedLanguages,
+      enableSandbox: parsedEnableSandox,
+    };
+
+    const validationResult = CreateInterviewSchema.safeParse(payloadToValidate);
+    if (!validationResult.success) {
+      res.status(400).json({
+        error: "Validation failed",
+        details: validationResult.error.format(),
+      });
+      return;
+    }
+
+    const {
+      candidateId,
+      roleTitle,
+      jobDescription,
+      difficultyMode = "medium",
+      enableSandbox,
+      allowedLanguages,
+      targetSkills,
+    } = validationResult.data;
+
+    let extractedResumeText = "";
+    if (req.file) {
+      const unitArrayData = new Uint8Array(req.file.buffer);
+      const textData = await extractTextFromArrayBuffer(unitArrayData);
+
+      if (!textData) {
+        console.warn(
+          "Warning: extractTextFromArrayBuffer returned empty or null data.",
+        );
+      } else {
+        extractedResumeText = textData;
+      }
+    }
+    const interview = await InterviewModel.create({
+      candidateId,
+      roleTitle,
+      targetSkills: parsedSkills || [],
+      resumeText: extractedResumeText,
+      jobDescription,
+      difficultyMode,
+      enableSandbox,
+      allowedLanguages,
+      status: "scheduled",
+    });
+
+    res.status(201).json({
+      message: "Interview session created successfully with uploaded resume.",
+      interviewId: interview._id,
+      config: {
+        difficultyMode: interview.difficultyMode,
+        enableSandbox: interview.enableSandbox,
+        allowedLanguages: interview.allowedLanguages,
+      },
+    });
+  } catch (error: any) {
+    console.error("Error in createInterviewSession: ", error.message);
+    res.status(500).json({
+      error: "Failed to create interview session",
+      details: error.message,
+    });
+  };
+};
