4002bc878e7a2087a31e16d37ac96864ddce06e9
dushant <dushantbanpurkar@gmail.com>
2026-09-13 19:54:13 +0530

    lots mistake i have done in model like score:string, missing Lean Reject and Most important Candiate spelling to Candidate. I should more aware in typing.

---
+++ src/model/candidateReport.model.ts
@@ -0,0 +1,116 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface ICandidateReport extends Document {
+  interviewId: string;
+  executiveSummary: string;
+  overallScore: number;
+  recommendation:
+    | "Strong Hire"
+    | "Hire"
+    | "Lean Hire"
+    | "Lean Reject"
+    | "Reject";
+
+  technicalEvaluation?: {
+    score: number;
+    problemSolvingScore: number;
+    codeQualtiyScore: number;
+    correctnessScore: number;
+    codeAnalysis: string;
+    strengths: string[];
+    improvements: string[];
+  };
+  communicationEvaluation?: {
+    score: number;
+    clarityAndStructure: string;
+    deliveryAndConfidence: string;
+    fillerWordsObserved: string[];
+  };
+
+  questionBreakdown: Array<{
+    questionText: string;
+    candidateResponseSummary: string;
+    score: number;
+    keyTakeaway: string;
+  }>;
+
+  redFlags: string[];
+  recommendedFollowUps: string[];
+}
+const CandidateReportSchema = new Schema(
+  {
+    interviewId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    executiveSummary: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    overallScore: {
+      type: Number,
+      min: 0,
+      max: 100,
+      required: true,
+    },
+    recommendation: {
+      type: String,
+      enum: {
+        values: ["Strong Hire", "Hire", "Lean Hire", "Lean Reject", "Reject"],
+        message: "{VALUE} is not a valid recommendation option",
+      },
+      required: true,
+      trim: true,
+    },
+    technicalEvaluation: {
+      type: {
+        score: { type: Number, min: 1, max: 100 },
+        problemSolvingScore: { type: Number, min: 1, max: 10 },
+        codeQualtiyScore: { type: Number, min: 1, max: 10 },
+        correctnessScore: { type: Number, min: 1, max: 10 },
+
+        codeAnalysis: { type: String, trim: true },
+        strengths: [String],
+        improvements: [String],
+      },
+      default: undefined,
+      required: true,
+    },
+    communicationEvaluation: {
+      type: {
+        score: { type: Number, min: 1, max: 10 },
+        clarityAndStructure: { type: String, trim: true },
+        deliveryAndConfidence: { type: String, trim: true },
+        fillerWordsObserved: [String],
+      },
+      default: undefined,
+    },
+
+    questionBreakdown: [
+      {
+        questionText: { type: String, required: true },
+        candidateResponseSummary: { type: String, required: true },
+        score: { type: Number, min: 1, max: 10, required: true },
+        keyTakeaway: { type: String, required: true },
+      },
+    ],
+    redFlags: {
+      type: [String],
+      default: undefined,
+    },
+    recommendedFollowUps: {
+      type: [String],
+      default: undefined,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const CandidateReportModel: Model<ICandidateReport> =
+  mongoose.models.CandiateReport ||
+  mongoose.model<ICandidateReport>("CandiateReport", CandidateReportSchema);
