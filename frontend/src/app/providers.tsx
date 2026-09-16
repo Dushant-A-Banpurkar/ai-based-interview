e1bb82c9991f448c6f8f8f5e8f6f06ba2b669861
dushant <dushantbanpurkar@gmail.com>
2026-09-16 23:45:51 +0530

    provider commonent wrap client -side state providers so server components remain unblocked.

---
+++ frontend/src/app/providers.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import {Provider as ReduxProvider} from 'react-redux';
+import {QueryClient,QueryClientProvider} from '@tanstack/react-query';
+import {useState} from 'react';
+import {store} from '@store/store'
+
+
+export default function Providers({children}:{children:React.ReactNode}){
+    const [queryClient]=useState(()=>{
+        return new QueryClient({
+            defaultOptions:{
+                queries:{
+                    refetchOnWindowFocus:false,
+                    retry:1
+                }
+            }
+        })
+    })
+    return(
+        <ReduxProvider store={store}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </ReduxProvider>
+    )
+};
