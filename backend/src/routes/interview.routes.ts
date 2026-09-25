539ccf5dcdbec3fbe200d88642c6a3ed3dcab0b2
dushant <dushantbanpurkar@gmail.com>
2026-09-25 19:41:23 +0530

    upload memory added as middleware

--- backend/src/routes/interview.routes.ts
+++ backend/src/routes/interview.routes.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express';
 import { createInterviewSession, endInterviewSession, getInterviewReport, getInterviewStatus } from '../controller/interview.controller';
+import { uploadMemory } from '../middlewares/uploadMemory';
 
 const router=Router();
 
-router.post('/createinterviewsession',createInterviewSession);
+router.post('/createinterviewsession',uploadMemory.single("pdf"),createInterviewSession);
 
 router.post('/:interviewId/end',endInterviewSession);
 
